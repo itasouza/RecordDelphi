3c548269f2de72c7569fda2ae4e8a1c27182d39c
itasouza <itasouza@yahoo.com.br>
2014-10-20 10:54:42 -0200

    Usando record com Delphi

---
+++ Unit1.dfm
@@ -0,0 +1,86 @@
+object Form1: TForm1
+  Left = 58
+  Top = 111
+  Width = 527
+  Height = 334
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 280
+    Top = 144
+    Width = 32
+    Height = 13
+    Caption = 'Label1'
+  end
+  object SpeedButton1: TSpeedButton
+    Left = 432
+    Top = 80
+    Width = 23
+    Height = 22
+  end
+  object Button1: TButton
+    Left = 144
+    Top = 64
+    Width = 75
+    Height = 25
+    Caption = 'grava'#231#227'o'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Edit1: TEdit
+    Left = 272
+    Top = 40
+    Width = 121
+    Height = 21
+    TabOrder = 1
+  end
+  object Edit2: TEdit
+    Left = 272
+    Top = 72
+    Width = 121
+    Height = 21
+    TabOrder = 2
+  end
+  object Edit3: TEdit
+    Left = 272
+    Top = 104
+    Width = 121
+    Height = 21
+    TabOrder = 3
+  end
+  object Button2: TButton
+    Left = 144
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'leitura'
+    TabOrder = 4
+    OnClick = Button2Click
+  end
+  object BitBtn1: TBitBtn
+    Left = 384
+    Top = 136
+    Width = 75
+    Height = 25
+    Caption = 'listar'
+    TabOrder = 5
+    OnClick = BitBtn1Click
+  end
+  object Memo1: TMemo
+    Left = 48
+    Top = 176
+    Width = 425
+    Height = 89
+    Lines.Strings = (
+      '')
+    TabOrder = 6
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,114 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Buttons;
+
+type
+   //conjunto de variáveis
+   TDADOS  = Record
+     id:Integer;
+     nome, email, sexo:String;
+end;
+
+type
+   //conjunto de variáveis
+   TDADOS_CLIENTE  = Record
+     id:Integer;
+     nome, email, sexo:String;
+end;
+
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Button2: TButton;
+    Label1: TLabel;
+    SpeedButton1: TSpeedButton;
+    BitBtn1: TBitBtn;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+
+    procedure BitBtn1Click(Sender: TObject);
+    //adicionar dados
+    procedure AdicionarDados(d : TDADOS);
+  private
+    { Private declarations }
+    //declara a variável com o tipo do record
+    //neste caso grava sempre o último registro
+    Dados :TDADOS;
+    CLIENTE :TDADOS_CLIENTE;
+
+    //array para adicionar os dados
+    //neste caso grava vários registros
+    arDados : Array of TDADOS;
+
+
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+
+procedure TForm1.AdicionarDados(d: TDADOS);
+begin
+  SetLength(arDados,Length(arDados)+1);
+  arDados[Length(arDados)-1] := D;
+end;
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+   //gravando record
+
+   Dados.id   := Dados.id + 1;
+   Dados.nome := Edit1.Text;
+   Dados.email:= Edit2.Text;
+   Dados.sexo := Edit3.Text;
+   AdicionarDados(Dados);
+
+   //limpar campos
+   Edit1.Text := '';
+   Edit2.Text := '';
+   Edit3.Text := '';
+
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+    //leitura do record (neste caso o último)
+    Label1.Caption := IntToStr(Dados.id);
+    Edit1.Text := Dados.nome;
+    Edit2.Text := Dados.email;
+    Edit3.Text := Dados.sexo;
+end;
+
+procedure TForm1.BitBtn1Click(Sender: TObject);
+  var
+  s:TStringList;
+  i:Integer;
+begin
+    //leitura de vários registros inseridos no record
+   for i:= 0 to Pred(length(arDados)) do
+    begin
+       Memo1.Lines.Add(IntToStr(arDados[i].id) + '-' + arDados[i].nome +
+                                                 '-' + arDados[i].sexo +
+                                                 '-' + arDados[i].email);
+    end;
+
+end;
+
+end.
